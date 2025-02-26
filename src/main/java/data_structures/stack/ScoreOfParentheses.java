66c571c118d475f1416af15702ce640d4bdd0046
LuyaoWang123 <lwang773@wisc.edu>
2025-02-25 23:15:20 -0600

    [DSA-6] finish code

---
+++ src/main/java/data_structures/stack/ScoreOfParentheses.java
@@ -0,0 +1,20 @@
+package src.main.java.data_structures.stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class ScoreOfParentheses {
+    public int scoreOfParentheses(String s) {
+        Deque<Integer> score = new ArrayDeque<>();
+        score.push(0);
+        for (char parenthesis: s.toCharArray()) {
+            if (parenthesis == '(') score.push(0);
+            else {
+                int matchVal = score.pop();
+                int curVal = score.pop();
+                score.push(curVal + Math.max(2 * matchVal, 1));
+            }
+        }
+        return score.peek();
+    }
+}
---
+++ src/test/java/data_structures/stack/ScoreOfParenthesesTest.java
@@ -0,0 +1,52 @@
+package src.test.java.data_structures.stack;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import src.main.java.data_structures.stack.ScoreOfParentheses;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ScoreOfParenthesesTest {
+
+    private ScoreOfParentheses scoreOfParentheses;
+
+    @BeforeEach
+    public void setUp() {
+        scoreOfParentheses = new ScoreOfParentheses();
+    }
+
+    @Test
+    public void testEmptyStringScore() {
+        assertEquals(0, scoreOfParentheses.scoreOfParentheses(""));
+    }
+
+    @Test
+    public void testSinglePairScore() {
+        assertEquals(1, scoreOfParentheses.scoreOfParentheses("()"));
+    }
+
+    @Test
+    public void testAdjacentPairsScore() {
+        assertEquals(2, scoreOfParentheses.scoreOfParentheses("()()"));
+    }
+
+    @Test
+    public void testNestedPairScore() {
+        assertEquals(2, scoreOfParentheses.scoreOfParentheses("(())"));
+    }
+
+    @Test
+    public void testMixedAdjacentAndNestedScore() {
+        assertEquals(3, scoreOfParentheses.scoreOfParentheses("()(())"));
+    }
+
+    @Test
+    public void testComplexNestedParenthesesScore() {
+        assertEquals(6, scoreOfParentheses.scoreOfParentheses("(()(()))"));
+    }
+
+    @Test
+    public void testDeeplyNestedParenthesesScore() {
+        assertEquals(4, scoreOfParentheses.scoreOfParentheses("((()))"));
+    }
+}
